d294b9e726deeb00ab61cbeee827b71fdc13dfbc
ufcyg <mellowski@gmail.com>
2021-04-28 14:46:43 +0200

    refactoring

--- src/Subscriber/ProductEventSubscriber.php
+++ src/Subscriber/ProductEventSubscriber.php
@@ -29,8 +29,7 @@ class ProductEventSubscriber implements EventSubscriberInterface
         // Return the events to listen to as array like this:  <event to listen to> => <method to execute>
         return [
             'product.written' => 'onProductWrittenEvent',
-            'product.deleted' => 'onProductDeletedEvent',
-            'order.written' => 'onOrderWrittenEvent'
+            'product.deleted' => 'onProductDeletedEvent'
         ];
     }
 
@@ -38,21 +37,6 @@ class ProductEventSubscriber implements EventSubscriberInterface
     {
         $eventArray = $event->getWriteResults();
         $this->asDispoController->upsertDispoControlEntry($eventArray[0]->getPrimaryKey(), Context::createDefaultContext());
-    }
-
-    public function onOrderWrittenEvent(EntityWrittenEvent $event)
-    {
-        // $this->asDispoController->initDispoControlData(Context::createDefaultContext());
-
-        // $eventArray = $event->getWriteResults();
-        // $orderID = $eventArray[0]->getPrimaryKey();
-        // $payload = $eventArray[0]->getPayload();
-        // if(array_key_exists('stateId', $payload))
-        // {
-        //     $newStateID = $payload['stateId'];
-        //     $this->asDispoController->updateOrderStatusChange($orderID,$newStateID);
-        //     $this->asDispoController->checkThresholds(Context::createDefaultContext());
-        // }
     }
 
     public function onProductDeletedEvent(EntityWrittenEvent $event)
